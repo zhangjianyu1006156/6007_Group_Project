a129a5d09dbc12e66d85a9c4a0c495edc72d075f
cliu418-ops <cliu418@aucklanduni.ac.nz>
2026-01-22 16:18:46 +0800

    Delete backend/storage/memory_store.py

--- backend/storage/memory_store.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict
-from server.models.voucher import Voucher
-from server.models.reservation import Reservation
-
-class MemoryStore:
-    def __init__(self) -> None:
-        self.vouchers: Dict[str, Voucher] = {}
-        self.reservations: Dict[str, Reservation] = {}
